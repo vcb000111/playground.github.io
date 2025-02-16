a6ffc605091bc4b742972d2f5490b1d1ca23748f
Minh <vcb000111@gmail.com>
2025-02-16 09:54:12 +0700

    Add assetPrefix to fix static files loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/playground',
+  assetPrefix: '/playground/',
   images: {
     unoptimized: true
   }
