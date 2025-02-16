3cedb655a50541279ca02ba5b8cdcb92bf4ea545
Minh <vcb000111@gmail.com>
2025-02-16 09:56:56 +0700

    Configure dynamic asset prefix and base path for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = process.env.GITHUB_REPOSITORY || '';
+
+const assetPrefix = isGithubActions ? `/${repo.split('/')[1]}` : '';
+const basePath = isGithubActions ? `/${repo.split('/')[1]}` : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/playground',
-  assetPrefix: '/playground/',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  trailingSlash: true,
   images: {
     unoptimized: true
   }
